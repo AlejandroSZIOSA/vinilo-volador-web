080db99c5a9331cf6a4acc6d1676892013f4f06a
AlejandroSZIOSA <ggato33@gmail.com>
2025-05-09 17:31:24 +0200

    Context Add remove is working with reducers

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,24 @@
 import { type FC } from "react";
 import { useAdmin_Ctx } from "../store/admin-Context";
+import { type Vinyl } from "../types/shared";
+
+const testNewVinyl: Vinyl = {
+  id: "1",
+  album: "Test Album 1",
+  artist: "Test Artist1",
+};
 
 const HomePage: FC = () => {
-  const { vinyls } = useAdmin_Ctx();
+  const vinyls = useAdmin_Ctx();
 
-  console.log(vinyls);
+  /* console.log(vinyls.vinyls); */
   return (
-    <>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <h1>home Page</h1>
-    </>
+      <button onClick={() => vinyls.add_Vinyl(testNewVinyl)}>ADD VINYL</button>
+      <button onClick={() => console.log(vinyls)}>show vinyls</button>
+      <button onClick={() => vinyls.remove_Vinyl("1")}>Remove</button>
+    </div>
   );
 };
 
--- src/store/admin-Context.tsx
+++ src/store/admin-Context.tsx
@@ -11,8 +11,11 @@ import { VINYLS, NEXT_EVENT } from "../data/static-data";
 interface AdminContextType {
   vinyls: Vinyl[] | null;
 
-  /* updatedList: Date;
-  nextEvent: Event; */
+  nextEvent: Event;
+  updatedList: Date;
+
+  add_Vinyl: (v: Vinyl) => void;
+  remove_Vinyl: (v: string) => void;
 
   /* is_Authenticated: boolean | string;
   login_Fn: (token: string) => void;
@@ -20,25 +23,55 @@ interface AdminContextType {
 }
 
 //Reducers
-/* function VinilsReducer(state: Vinyl[] | null, action: any) {
 
+//discriminant types
+type ADD_VINYL = {
+  type: "ADD_VINYL";
+  payload: Vinyl;
+};
 
+type REMOVE_VINYL = {
+  type: "REMOVE_VINYL";
+  payload: string;
+};
 
-} */
+type Action = ADD_VINYL | REMOVE_VINYL; // discriminant union
+
+function vinylsReducer(state: Vinyl[] | null, action: Action): Vinyl[] {
+  switch (action.type) {
+    case "ADD_VINYL":
+      return state ? [...state, action.payload] : [action.payload];
+    case "REMOVE_VINYL":
+      return state ? state.filter((vinyl) => vinyl.id !== action.payload) : [];
+    /* case 'UPDATE_VINYL':
+      return state ? state.map(vinyl => vinyl.id === action.payload.id ? action.payload : vinyl) : []; */
+    default:
+      return state || [];
+  }
+}
 
 const AdminContext = createContext<AdminContextType | null>(null);
 
 export const AdminProvider_Ctx = ({ children }: { children: ReactNode }) => {
-  const [vinyls, setVinyls] = useState<Vinyl[] | null>(VINYLS);
+  /*   const [vinyls, setVinyls] = useState<Vinyl[] | null>(VINYLS);
+   */
 
-  /* useReducer(reducer,initialState) */
+  const [vinylsState, dispatch] = useReducer(vinylsReducer, VINYLS);
 
   /*  const login_Fn = (t: string) => setAuthenticated(t); 
   const logout_Fn = () => setAuthenticated(false); */
 
-  return (
-    <AdminContext.Provider value={{ vinyls }}>{children}</AdminContext.Provider>
-  );
+  const ctx: AdminContextType = {
+    vinyls: vinylsState,
+    add_Vinyl(v) {
+      dispatch({ type: "ADD_VINYL", payload: v });
+    },
+    remove_Vinyl(v) {
+      dispatch({ type: "REMOVE_VINYL", payload: v });
+    },
+  };
+
+  return <AdminContext.Provider value={ctx}>{children}</AdminContext.Provider>;
 };
 
 export const useAdmin_Ctx = () => {
