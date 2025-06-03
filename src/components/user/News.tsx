da76fd11f96221876e7a46f4d607115b4a9c1f98
AlejandroSZIOSA <ggato33@gmail.com>
2025-06-03 20:53:52 +0200

    user page , home and news components are ready

--- src/components/user/News.tsx
+++ src/components/user/News.tsx
@@ -7,37 +7,42 @@ const News: FC = () => {
   const { next_Event } = useAdmin_Ctx();
   return (
     <section className={classes.newsSectionContainer}>
-      <h1>Next Pick up place</h1>
-      <div className={classes.newPlaceContainer}>
-        <div className={classes.newPlaceInnerContainer}>
-          <div>
-            <p>
-              <strong>Place:</strong> {next_Event?.place}
+      <div className={classes.pickUpPlaceContainer}>
+        <h1>Next Pick up place</h1>
+        <div className={classes.newPlaceInfoOuterContainer}>
+          <div className={classes.newPlaceInfoInnerContainer}>
+            <div>
+              <p>
+                <strong>Place:</strong> {next_Event?.place}
+              </p>
+              <p>
+                <strong>Address:</strong> {next_Event?.address}
+              </p>
+              <p>
+                <strong>Date:</strong> {next_Event?.date}
+              </p>
+            </div>
+            <p style={{ textAlign: "center" }}>
+              <strong>Time</strong>
             </p>
-            <p>
-              <strong>Address:</strong> {next_Event?.address}
-            </p>
-            <p>
-              <strong>Date:</strong> {next_Event?.date}
-            </p>
-          </div>
-          <p style={{ textAlign: "center" }}>
-            <strong>Time</strong>
-          </p>
-          <div className={classes.timeContainer}>
-            <p>
-              <strong>From:</strong> {next_Event?.from}
-            </p>
-            <p>
-              <strong>To:</strong>
-              {next_Event?.to}
-            </p>
+            <div className={classes.timeContainer}>
+              <p>
+                <strong>From:</strong> {next_Event?.from}
+              </p>
+              <p>
+                <strong>To:</strong>
+                {next_Event?.to}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <h1>Latest Added Items</h1>
-      <div className={classes.tableShowOuterContainer}>
-        <TableListShow variant="user-latest" />
+
+      <div className={classes.latestAddedItemsContainer}>
+        <h1>Latest Added Items</h1>
+        <div className={classes.tableShowOuterContainer}>
+          <TableListShow variant="user-latest" />
+        </div>
       </div>
     </section>
   );
